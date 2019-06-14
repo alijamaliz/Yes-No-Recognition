aec63bc705175e75510f3f859ab8cceec9c7fca9
Ali <jamaliali219@gmail.com>
2019-06-14 13:03:03 +0430

    Apply new recognition algorithm

--- yes-no.py
+++ yes-no.py
@@ -1,5 +1,6 @@
 from scipy.io import wavfile as wav
 from scipy.fftpack import fft
+from scipy.signal import argrelextrema
 import pyaudio
 import wave
 import numpy as np
@@ -8,72 +9,83 @@ import matplotlib.pyplot as plot
 from os import listdir
 from os.path import isfile, join, exists
 
+effectiveRange = 600
 
-def distanceOfTwoArrays(first, second):
+def isSignalMortal(signal):
+    section1 = 0
+    for i in range(0, int(len(signal) / 3)):
+        section1 += signal[i]
+    section2 = 0
+    for i in range(int(len(signal) / 3), int(2 * len(signal) / 3)):
+        section2 += signal[i]
+    section3 = 0
+    for i in range(int(2 * len(signal) / 3), len(signal)):
+        section3 += signal[i]
+    if (section1 > section2 and section2 > section3 and section1):
+        if (signalFirstPeakValue(signal) > 10000):
+            return True;
+    return False;
+
+def signalFirstPeakValue(signal):
     res = 0
-    smallArray = first
-    largeArray = second
-    if len(first) > len(second):
-        smallArray = second
-        largeArray = first
-    
-    for i in range(len(largeArray) - len(smallArray)):
-        smallArray.append(0)
-
-    for i in range(len(largeArray)):
-        res += (smallArray[i] - largeArray[i])
-
+    for i in range(0, int(len(signal) / 6)):
+        if (signal[i] > res):
+            res = signal[i]
     return res
 
+def energyOfSignal(signal):
+    res = 0
+    for i in range(0, len(signal)):
+        res += signal[i]**2
+    return res
 
-if (not exists('learned')):
-    print("error: no learned data found!")
-    exit()
+def signalExtermasCount(signal):
+    greater = len(argrelextrema(np.asarray(limitedFourierTransformOfData), np.greater)[0])
+    less = len(argrelextrema(np.asarray(limitedFourierTransformOfData), np.less)[0])
+    return greater + less
 
-# Read learned data
-print("Reading learned data")
-leadrnedgFilesDirectory = 'learned/'
-learnedFiles = listdir(leadrnedgFilesDirectory)
-knowledge = {}
-for learnedFile in learnedFiles:
-    path = join(leadrnedgFilesDirectory, learnedFile)
-    word = learnedFile[0:-4]
-    file = open(path, 'r')
-    for line in file.readlines():
-        if word in knowledge:
-            knowledge[word].append(int(line))
+def recogniseYesNo(signal): 
+    if (isSignalMortal(signal)):
+        if (signalFirstPeakValue(signal) > 100000):
+            return True
         else:
-            knowledge[word] = [int(line)]
-print("Finished")
+            return False
+    else:
+        if (energyOfSignal(limitedFourierTransformOfData) > 10000000000): 
+            return False
+        else:
+            if (signalExtermasCount(limitedFourierTransformOfData) < 300):
+                return False
+            else:
+                return True
 
-p1 = []
-p2 = []
-for i in range(500):
-    p1.append(knowledge['yes'][i])
-    p2.append(knowledge['no'][i])
-plot.plot(p1, 'b', label='yes')
-plot.plot(p2, 'r', label='no')
-plot.legend()
+correctAnswers = 0
+trainingFilesDirectory = 'train/'
+trainingFiles = listdir(trainingFilesDirectory)
+index = 1
+for file in trainingFiles:
+    path = join(trainingFilesDirectory, file)
+    if isfile(path) and 'wav' in path:
+        rate, data = wav.read(path)
+        data = np.array(data,dtype='int')
+        FourierTransformOfData = np.fft.fft(data, 44100)
+        FourierTransformOfData[0] = 0
+        for i in range(effectiveRange):
+            FourierTransformOfData[i] = int(np.absolute(FourierTransformOfData[i]))
 
-plot.show()
+        limitedFourierTransformOfData = []
+        for i in range(effectiveRange):
+            limitedFourierTransformOfData.append(FourierTransformOfData[i])
 
+        word = ''.join([i for i in file[0:-5] if not i.isdigit()]) # Extract word from file name
+        recognised = ""
+        if (recogniseYesNo(limitedFourierTransformOfData)):
+            recognised = "yes"
+        else:
+            recognised = "no"
+        if (recognised == word):
+            correctAnswers += 1
+        print('{}: \033[93m{}\033[0m -> \033[92m{}\033[0m -> {}'.format(index, path, word, recognised))
+        index += 1
 
-# rate, data = wav.read('file.wav')
-rate, data = wav.read('train/no10.wav')
-FourierTransformOfData = np.fft.fft(data, 44100)
-
-# Convert fourier transform complex number to integer numbers
-for i in range(len(FourierTransformOfData)):
-    FourierTransformOfData[i] = int(np.absolute(FourierTransformOfData[i]))
-
-minimumDistanceKey = ""
-minimumDistanceValue = -1
-for word in knowledge.keys():
-    temp = distanceOfTwoArrays(FourierTransformOfData, knowledge[word])
-    if (temp < minimumDistanceValue or minimumDistanceValue == -1):
-        minimumDistanceValue = temp
-        minimumDistanceKey = word
-
-print(minimumDistanceKey)
-print(distanceOfTwoArrays(FourierTransformOfData, knowledge['yes']))
-print(distanceOfTwoArrays(FourierTransformOfData, knowledge['no']))
+print ('Number of correct recognitions: {} ({}%)'.format(correctAnswers, (correctAnswers / (index - 1)) * 100))
